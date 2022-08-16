feed95e6cc2642840bc1673bb58b9b1362f59038
Ильнур <ilnursafin13312@gmail.com>
2022-08-16 22:11:10 +0300

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     int age = 2021;
     public static void main(String[] args) {
         printLeapAge(2020);
-        outputApplicationVersionOS(1,2022);
-        calculateDeliveryDays(99);
+        outputApplicationVersionOS(0,2021);
+        System.out.println(calculateDeliveryDays(99) + " " + "дня/дней будет доставка");
     }
     private static void printLeapAge(int age){
 
@@ -17,7 +17,8 @@ public class Main {
         }
     private static void outputApplicationVersionOS(int clientOs , int age1){
         int currentYear = LocalDate.now().getYear();
-        if (age1 > currentYear) {
+        /*
+        if (age1 >= currentYear ) {
             if (clientOs == 0) {
                 System.out.println("Установите версию для IOS");
             } else {
@@ -27,23 +28,28 @@ public class Main {
             System.out.println("Установите облегченную версию для IOS");
         } else {
             System.out.println("Установите облегченную версию для Android");
+*/
+        if (clientOs == 0 && age1 >= currentYear){
+             System.out.println("Установите версию для IOS");}
+       else if (age1 >= currentYear){
+           System.out.println("Установите версию для Android");}
+       else if (clientOs == 0){
+           System.out.println("Установите облегченную версию для IOS");}
+       else
+         System.out.println("Установите облегченную версию для Android");
 
-        }
     }
 
     private static int calculateDeliveryDays (float distance) {
         int day = 0;
         if (distance <= 20){
             day = day + 1;
-            System.out.println(day + " день будет доставка");
         }
         else if (distance<60 && distance>20){
             day = day + 2;
-            System.out.println(day + " дня будет доставка");
         }
         else if (distance<100){
             day= day + 3;
-            System.out.println(day + " дня/дней будет доставка");
         }
         return day;
     }
